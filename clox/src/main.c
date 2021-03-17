1e6841e1a6095414d92953f4b8b55fb44117cfa3
ProfSucrose <dr.blobbles@gmail.com>
2021-03-17 11:30:19 -0700

    Starting work on clox VM

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,13 +1,19 @@
 #include "common.h"
 #include "chunk.h"
-#include <stdio.h>
+#include "debug.h"
+#include "value.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     init_chunk(&chunk);
-    write_chunk(&chunk, OP_RETURN);
+
+    int constant = add_constant(&chunk, 1.2);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+
+    write_chunk(&chunk, OP_RETURN, 123);
+
+    disassemble_chunk(&chunk, "test chunk");
     free_chunk(&chunk);
-    printf("Successfully created and freed chunk\n");
-    printf("Hello World!\n");
     return 0;
 }
---
+++ clox/src/vm.c
@@ -0,0 +1,13 @@
+#include "common.h"
+#include "vm.h"
+
+VM vm;
+
+// continue on implementing the VM tomorrow!
+void initVM() {
+
+}
+
+void freeVM() {
+
+}
